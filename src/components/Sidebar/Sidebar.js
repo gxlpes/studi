e0e50a75129727ef7a51fede4fa1ee830c262577
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-03 21:21:50 -0300

    Finished styling sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { SidebarContainer, Icon, CloseIcon } from "./SidebarStyles";
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from "./SidebarStyles";
 
 const Sidebar = () => {
   return (
@@ -14,7 +23,11 @@ const Sidebar = () => {
             <SidebarLink to="cases">Cases</SidebarLink>
             <SidebarLink to="services">Services</SidebarLink>
             <SidebarLink to="reviews">Reviews</SidebarLink>
+            <SidebarLink to="signup">Sign Up</SidebarLink>
           </SidebarMenu>
+          <SideBtnWrap>
+            <SidebarRoute to="/signin">Sign In</SidebarRoute>
+          </SideBtnWrap>
         </SidebarWrapper>
       </SidebarContainer>
     </>
--- src/components/Sidebar/SidebarStyles.js
+++ src/components/Sidebar/SidebarStyles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { Link as LinkScroll } from "react-scroll";
+import { Link as LinkRouter } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 
 export const SidebarContainer = styled.aside`
@@ -12,8 +14,8 @@ export const SidebarContainer = styled.aside`
   top: 0;
   left: 0;
   transition: 0.3s ease-in-out;
-  /* opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
-  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")}; */
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
 `;
 
 export const CloseIcon = styled(FaTimes)`
@@ -27,4 +29,63 @@ export const Icon = styled.div`
   background: transparent;
   cursor: pointer;
   outline: none;
+`;
+
+export const SidebarMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    grid-template-rows: repeat() (6, 60px);
+  }
+`;
+
+export const SidebarWrapper = styled.div`
+  color: #fff;
+`;
+
+export const SidebarLink = styled(LinkScroll)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  color: #23272f;
+  cursor: pointer;
+
+  &:hover {
+    color: #01bf71;
+    transition: 0.2s ease-in-out;
+  }
+`;
+
+export const SideBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+`;
+
+export const SidebarRoute = styled(LinkRouter)`
+  color: white;
+  border-radius: 50px;
+  background: purple;
+  white-space: nowrap;
+  padding: 0.5rem 2rem;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+  }
 `;
