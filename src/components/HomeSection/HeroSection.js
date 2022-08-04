46d38484fbe0c4480103bf7388720a7177d7f7dd
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-03 22:27:17 -0300

    Added button to reuse

--- src/components/HomeSection/HeroSection.js
+++ src/components/HomeSection/HeroSection.js
@@ -1,13 +1,41 @@
-import React from "react";
 import { ReactComponent as SVGFile } from "../../images/HeroImage.svg";
-import { HeroContainer, HeroBg, SVGImage } from "./HeroSectionStyles";
+import {
+  HeroContainer,
+  HeroBg,
+  SVGImage,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroSectionStyles";
+import { Button } from "../ButtonElement";
+import { useState } from "react";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer id="home">
       <HeroBg>
-        <SVGFile></SVGFile>
+        <SVGImage>
+          <SVGFile></SVGFile>
+        </SVGImage>
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Learn new techniques to study better.</HeroH1>
+        <HeroP>Sign up for a new account today and try our services!</HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
